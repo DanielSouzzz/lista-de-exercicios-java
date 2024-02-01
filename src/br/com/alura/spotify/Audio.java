5a9ad8afb130eb110fe9c76fe7c4ff82bfd9bc24
Carlos Daniel de Souza Cordeiro <134225062+DanielSouzzz@users.noreply.github.com>
2024-02-01 20:53:09 -0300

    novo execercio

---
+++ src/br/com/alura/spotify/Audio.java
@@ -0,0 +1,12 @@
+package br.com.alura.spotify;
+import java.time.Duration;
+
+public class Audio {
+    private String titulo;
+    private int duracao;
+    private double totalDeReproducoes;
+    private int curtidas;
+    private int classificacaoEmEstrelas;
+
+
+}
