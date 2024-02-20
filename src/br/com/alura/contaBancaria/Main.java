da11e0ee59db853526a49a7988e098dd744dda22
Carlos Daniel de Souza Cordeiro <134225062+DanielSouzzz@users.noreply.github.com>
2024-02-19 22:18:01 -0300

    implementei a lógica com foreach no exercicio de conta bancária.

--- src/br/com/alura/contaBancaria/Main.java
+++ src/br/com/alura/contaBancaria/Main.java
@@ -1,24 +1,38 @@
 package br.com.alura.contaBancaria;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
     CurrentAccount currentAc = new CurrentAccount("6397172-0", 527.11);
     currentAc.setClientName("Caroline Lafuente");
     currentAc.setTypeAccount(true);
     currentAc.setBank("Inter");
-        System.out.println(currentAc);
+        //System.out.println(currentAc);
 
-    var currentAc1 = new CurrentAccount("7628304-9",0.00);
+    var currentAc1 = new CurrentAccount("7628304-9",852.00);
     currentAc1.setClientName("Daniel Souza");
     currentAc1.setTypeAccount(true);
     currentAc1.setBank("Nubank");
 
-    var saving = new SavingsAccount("8810653-5", 5.50);
+    var saving = new SavingsAccount("8810653-5", 500.50);
     saving.setClientName("Carlonis Souza");
     saving.setTypeAccount(false);
     saving.setBank("Santander");
-        System.out.println(saving);
+        //System.out.println(saving);
 
+        ArrayList<AccountBank> bankList = new ArrayList<>();
+        bankList.add(currentAc);
+        bankList.add(currentAc1);
+        bankList.add(saving);
+
+        AccountBank highestBankBalance = bankList.get(0); // assumi que primeira conta é a que tem maior saldo
+        for (AccountBank account : bankList){ // para cada elemento contido em um array, uma instrução é dada
+            if (account.getAccountBalance() > highestBankBalance.getAccountBalance()){ // compara qual o maior saldo
+                highestBankBalance = account;
+            }
+        }
+                System.out.println(highestBankBalance.getAccountBalance());
 
     }
 }
