62f8cee2173cd61d0750a193313d27ea17ba3950
Carlos Daniel de Souza Cordeiro <134225062+DanielSouzzz@users.noreply.github.com>
2024-03-12 21:59:18 -0300

    primeiro commit do exercicio de consumir api do github.

---
+++ src/br/com/alura/github/Main.java
@@ -0,0 +1,13 @@
+package br.com.alura.github;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite o nome do usuário: ");
+        var busca = input.next();
+
+
+    }
+}
