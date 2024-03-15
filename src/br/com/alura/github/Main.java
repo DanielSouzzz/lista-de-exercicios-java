cc87216b504fb7cdaad500aad02b5c88cc71f97c
Carlos Daniel de Souza Cordeiro <134225062+DanielSouzzz@users.noreply.github.com>
2024-03-14 21:12:03 -0300

    Construi a requisição para buscar usuários do github usando sua api pública.

--- src/br/com/alura/github/Main.java
+++ src/br/com/alura/github/Main.java
@@ -1,13 +1,28 @@
 package br.com.alura.github;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  throws IOException, InterruptedException{
         Scanner input = new Scanner(System.in);
         System.out.println("Digite o nome do usuário: ");
         var busca = input.next();
 
+        String endpoint = "https://api.github.com/search/users?q=" + busca;
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endpoint))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        String json = response.body();
+        System.out.println(json);
 
     }
 }
